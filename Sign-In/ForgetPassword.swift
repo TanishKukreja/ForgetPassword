f81b0a0a94adb95aa2e6486d76f19f6a0a5c1afa
“TanishKukreja:wq:qcd <tanishkukreja99@gmail.com>
2023-04-04 13:58:09 +0530

    forgetPassword

---
+++ Sign-In/ForgetPassword.swift
@@ -0,0 +1,29 @@
+//
+//  ForgetPassword.swift
+//  Sign-In
+//
+//  Created by Calsoft on 03/04/23.
+//
+
+import UIKit
+
+class ForgetPassword: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Sign-In/ViewController.swift
+++ Sign-In/ViewController.swift
@@ -8,11 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  
-    @IBOutlet weak var Email: UITextField!
-    @IBOutlet weak var Password: UITextField!
-    @IBOutlet weak var forgetPassword: UILabel!
     
     struct credentials {
         
@@ -20,6 +15,41 @@ class ViewController: UIViewController {
 //        var userpassword = "12345"
         
     }
+
+  
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var forgetPassword: UILabel!
+    @IBOutlet weak var forgetEmail: UITextField!
+    
+    
+    @IBAction func forgetButton(_ sender: Any) {
+        
+        var n = Int.random(in: 1000...9999)
+        
+        if(forgetEmail.hasText) {
+            let allertTitle = "Status"
+            let message = "Otp Sent to your mobile number"
+            let alertBox = UIAlertController(title: allertTitle, message: message, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "ok", style: .default , handler: nil)
+            alertBox.addAction(alertAction)
+            present(alertBox,animated: true,completion: nil)
+            print("Your Otp for verification is : \(n)")
+        }
+        
+            else  {
+            let allertTitle = "Status"
+            let message = "Enter Email for otp"
+            let alertBox = UIAlertController(title: allertTitle, message: message, preferredStyle: .alert)
+            
+            let alertAction = UIAlertAction(title: "ok", style: .default , handler: nil)
+            alertBox.addAction(alertAction)
+
+            present(alertBox,animated: true,completion: nil)
+        }
+    }
+    
+ 
    
     
     @IBAction func signButton(_ sender: Any) {
@@ -27,20 +57,42 @@ class ViewController: UIViewController {
          let user = Email.text
         let password = Password.text
         
-        if(user == "tanish.kukreja" && password == "1234") {
+        if  (user == "tanish.kukreja" && password == "1234" && Email.hasText && Password.hasText) {
             
             performSegue(withIdentifier: "segue1", sender: credentials())
             print(Email.text!)
             print("Logged in Successfully")
         }
-        else {
-            print("No match found")
+        
+        else if(user!.isEmpty && ((password?.isEmpty) != nil)){
+            
+            let allertTitle = "Status"
+            let message = "Enter Credentials"
+            let alertBox = UIAlertController(title: allertTitle, message: message, preferredStyle: .alert)
+            
+            let alertAction = UIAlertAction(title: "ok", style: .default , handler: nil)
+            alertBox.addAction(alertAction)
+
+            present(alertBox,animated: true,completion: nil)
+            print("")
+
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
+        else{
+             
+            let allertTitle = "Status"
+            let message = "Wrong Password"
+            let alertBox = UIAlertController(title: allertTitle, message: message, preferredStyle: .alert)
+            
+            let alertAction = UIAlertAction(title: "ok", style: .default , handler: nil)
+            alertBox.addAction(alertAction)
+
+            present(alertBox,animated: true,completion: nil)
+             
+            print("Wrong Password")
+        
+        }
+        
+        
     }
 }
 
